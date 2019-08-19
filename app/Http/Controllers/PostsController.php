c043e0aa8ec11a3f47b531e50770dd34b38f7c91
Oluwatobiloba <oluwatobilobaoyelowo@gmail.com>
2019-08-19 18:47:40 +0100

    Update controllers

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use function request as request;
+
+class PostsController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => ['required', 'image']
+        ]);
+
+       $imagePath =  request('image')->store('uploads', 'public');
+       $image = Image::make(public_path("storage/$imagePath"))->fit(820,600);
+       $image->save();
+
+       auth()->user()->posts()->create([
+          'caption' => $data['caption'],
+          'image' => $imagePath
+       ]);
+
+//       dd("ready to redirect");
+
+       return redirect('/profile/'. auth()->user()->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
