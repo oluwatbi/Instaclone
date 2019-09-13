50e7da2668bb0ab5466c59598dacc9bd11dc31c2
Oluwatobiloba <oluwatobilobaoyelowo@gmail.com>
2019-09-13 06:45:37 +0100

    Add posts@index view

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+    @foreach($posts as $post)
+        <div class="container">
+            <div class="row">
+                <div class="col-8 pb-5 offset-2">
+                    <div class="mr-3 w-100">
+                        <a href="/post/{{ $post->id }}">
+                            <img src="/storage/{{ $post->image }}" class="w-75">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="pr-3">
+                        <div class="d-flex align-items-center-center">
+                            <div class="pl-3">
+                                <img src="/storage/{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-width:50px;">
+                            </div>
+                            <div>
+                                <div class="font-weight-bold">
+                                    <a href="/profile/{{ $post->user->id }}">
+                                        <span class="text-dark">{{ $post->user->username }}</span></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <hr>
+                            <p>
+                            <span class="font-weight-bold">
+                                <a href="/profile/{{ $post->user->id }}">
+                                    <span class="text-dark">{{ $post->user->username }}</span>
+                                </a>
+                            </span>
+                                {{ $post->caption }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            {{ $posts->links() }}
+        </div>
+    </div>
+@endsection
